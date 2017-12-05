8334400475251f2b804f29e1832f29d06a9c7cf6
DartRabbit <dartrabbit02@gmail.com>
2017-12-05 18:38:50 +0300

    HW05 Optional - 7.1-7.3 Meal&Users OneToMany-ManyToOne service test

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,11 +1,25 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.MealTestData.assertMatch;
+import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.UserTestData.assertMatch;
 
 @ActiveProfiles(value = {Profiles.DATAJPA})
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
 
-
+    @Test
+    public void getWithUserTest() throws Exception {
+        Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+        User user = actual.getUser();
+        assertMatch(actual, ADMIN_MEAL1);
+        assertMatch(user, ADMIN);
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -1,11 +1,31 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static ru.javawebinar.topjava.MealTestData.MEALS;
+import static ru.javawebinar.topjava.MealTestData.assertMatch;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.assertMatch;
 
 @ActiveProfiles(value = {Profiles.DATAJPA})
 public class DataJpaUserServiceTest extends AbstractUserServiceTest {
 
-
+    @Test
+    public void testGetWithMeal() throws Exception {
+        User user = service.get(USER_ID);
+        Comparator<Meal> comparator = Comparator.comparing(Meal::getDateTime).reversed();
+        List<Meal> meals = user.getMeals();
+        meals.sort(comparator);
+        assertMatch(user, USER);
+        assertMatch(meals, MEALS);
+    }
 }
